1d9e5386ff79d3140a191c9b4c8ccfb9f456a03c
M Iqbal Al Islamy <iqbalal19@yahoo.com>
2026-02-24 20:25:49 +0700

    final railway fix

--- app.py
+++ app.py
@@ -317,12 +317,5 @@ def export(id):
 
     return send_file(filename, as_attachment=True)
 
-# ===== Railway / Gunicorn entry =====
-import os
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+with app.app_context():
+    db.create_all()
